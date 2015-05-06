3a5022c0d362058c83f8b54c3d2d9f1f8e0f36c3
Dennis Pattmann <d.pattmann@bigpoint.net>
2015-05-06 14:19:22 +0200

    Version Update 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'd.pattmann@bigpoint.net'
 license 'Apache 2.0'
 description 'Installs/Configures maxscale'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 depends 'apt'
 depends 'yum'
