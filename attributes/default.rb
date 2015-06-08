169f92595cae492ecc16067a57434ecb1fc68607
Dennis Pattmann <d.pattmann@bigpoint.net>
2015-06-08 17:11:32 +0200

    Config redesign

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,36 +32,11 @@ default['maxscale']['monitor']['detect_replication_lag'] = ''
 default['maxscale']['monitor']['detect_stale_master'] = ''
 default['maxscale']['monitor']['disable_master_failback'] = ''
 
-# Read Connection Router config
-default['maxscale']['read_connection_router']['enabled'] = true
-default['maxscale']['read_connection_router']['user'] = 'myuser'
-default['maxscale']['read_connection_router']['pwd'] = 'mypwd'
-default['maxscale']['read_connection_router']['option'] = 'slave'
-
-# RW Split Router conifg
-default['maxscale']['rw_split_router']['enabled'] = true
-default['maxscale']['rw_split_router']['user'] = 'myuser'
-default['maxscale']['rw_split_router']['pwd'] = 'mypwd'
-
-# read connection listener config
-default['maxscale']['read_connection_listener']['enabled'] = true
-default['maxscale']['read_connection_listener']['address'] = '127.0.0.1'
-default['maxscale']['read_connection_listener']['port'] = '4008'
-
-# rw split listener config
-default['maxscale']['rw_split_listener']['enabled'] = true
-default['maxscale']['rw_split_listener']['address'] = '127.0.0.1'
-default['maxscale']['rw_split_listener']['port'] = '4006'
-
-# debug listener config
-default['maxscale']['debug_listener']['enabled'] = true
-default['maxscale']['debug_listener']['address'] = '127.0.0.1'
-default['maxscale']['debug_listener']['port'] = '4442'
-
-# cli listener config
-default['maxscale']['cli_listener']['enabled'] = true
-default['maxscale']['cli_listener']['address'] = '127.0.0.1'
-default['maxscale']['cli_listener']['port'] = '6603'
+# Definition of the servers
+default['maxscale']['listener'] = []
+default['maxscale']['databag']['listener']['enabled'] = false
+default['maxscale']['databag']['listener']['name'] = 'maxscale'
+default['maxscale']['databag']['listener']['item_name'] = 'listener'
 
 # Definition of the servers
 default['maxscale']['server'] = []
@@ -74,3 +49,9 @@ default['maxscale']['filter'] = []
 default['maxscale']['databag']['filter']['enabled'] = false
 default['maxscale']['databag']['filter']['name'] = 'maxscale'
 default['maxscale']['databag']['filter']['item_name'] = 'filter'
+
+# Definition of the routers
+default['maxscale']['filter'] = []
+default['maxscale']['databag']['router']['enabled'] = false
+default['maxscale']['databag']['router']['name'] = 'maxscale'
+default['maxscale']['databag']['router']['item_name'] = 'router'
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -19,6 +19,8 @@
 
 all_filters = []
 all_servers = []
+all_listeners = []
+all_routers = []
 
 if node['maxscale']['databag']['filter']['enabled']
   # Load filter from Data Bags
@@ -38,12 +40,32 @@ else
   all_servers = node['maxscale']['server']
 end
 
+if node['maxscale']['databag']['listener']['enabled']
+  # Load listeners from Data Bags
+  databag_server = data_bag_item(node['maxscale']['databag']['listener']['name'], \
+                                 node['maxscale']['databag']['listener']['item_name'])
+  all_listeners = node['maxscale']['listener'] + databag_server['listener']
+else
+  all_listeners = node['maxscale']['listener']
+end
+
+if node['maxscale']['databag']['router']['enabled']
+  # Load listeners from Data Bags
+  databag_server = data_bag_item(node['maxscale']['databag']['router']['name'], \
+                                 node['maxscale']['databag']['router']['item_name'])
+  all_routers = node['maxscale']['router'] + databag_server['router']
+else
+  all_routers = node['maxscale']['router']
+end
+
 template '/usr/local/skysql/maxscale/etc/MaxScale.cnf' do
   source 'maxscale.cnf.erb'
   action :create
   notifies :restart, 'service[maxscale]', :delayed
   variables(
     maxscale_filters: all_filters,
-    maxscale_servers: all_servers
+    maxscale_servers: all_servers,
+    maxscale_listeners: all_listeners,
+    maxscale_routers: all_routers
     )
 end
